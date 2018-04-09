50363612905fc9a7ad69afd55272ff7ab4ec14d0
Bevan Wentzel <bevan@teamgeek.co.za>
2018-04-09 11:20:31 +0200

    Added some auth stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,20 @@ const IndexPage = ({ data }) => (
       </li>
     ))}
     </ul>
+
+    <script>{`
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        });
+      }
+    `}
+    </script>
+
   </div>
 )
 
